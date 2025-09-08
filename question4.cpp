fc9da96212aeebe6a04da99bb2220027ffa42d33
ee250002066-ops <ee250002066@iiti.ac.in>
2025-09-08 15:05:58 +0530

    Update question4.cpp

--- question4.cpp
+++ question4.cpp
@@ -4,10 +4,14 @@
 
 using namespace std;
 
-// TODO: Define TaylorTerm struct
-// struct TaylorTerm {
-// };
+// Define the TaylorTerm struct with order, coeff, and value
+struct TaylorTerm {
+    int order;    // The order of the term (2n+1)
+    double coeff; // The coefficient of the term (-1)^n / (2n+1)!
+    double value; // The value of the term (-1)^n * x^(2n+1) / (2n+1)!
+};
 
+// Function to print individual Taylor terms
 void print_taylor_term(int index, int order, double coeff, double value) {
     cout << "Term[" << index << "]: "
          << "order=" << order << ", "
@@ -15,12 +19,22 @@ void print_taylor_term(int index, int order, double coeff, double value) {
          << "value=" << value << "\n";
 }
 
+// Function to print the final Taylor series result
 void print_taylor_result(const char* function_name, double x,
                          int terms, double approximation) {
     cout << "Approximation of " << function_name
          << "(x) at x=" << x
          << " with " << terms << " terms: "
          << approximation << "\n";
+}
+
+// Function to compute factorial
+double factorial(int n) {
+    double result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
@@ -39,17 +53,29 @@ int main(int argc, char* argv[]) {
     int terms;
     input >> x >> terms;
 
-    // TODO: Create an array of TaylorTerm
-    // TaylorTerm series[terms];
+    // Create an array of TaylorTerm to store the terms
+    TaylorTerm series[terms];
 
     double approximation = 0.0;
 
-    // TODO: Compute each Taylor term and store in array
-    // for (int k = 0; k < terms; k++) {
-    // }
+    // Compute each Taylor term and store it in the array
+    for (int n = 0; n < terms; n++) {
+        int order = 2 * n + 1;  // Order of the term (2n+1)
+        double coeff = pow(-1, n) / factorial(order);  // Coefficient (-1)^n / (2n+1)!
+        double value = coeff * pow(x, order);  // Term value (-1)^n * x^(2n+1) / (2n+1)!
 
-    // TODO: Print final approximation
+        // Store in the series array
+        series[n] = {order, coeff, value};
 
+        // Add the value of the term to the approximation
+        approximation += value;
+
+        // Print the current term
+        print_taylor_term(n, series[n].order, series[n].coeff, series[n].value);
+    }
+
+    // Print the final approximation
+    print_taylor_result("sin", x, terms, approximation);
 
     return 0;
 }
